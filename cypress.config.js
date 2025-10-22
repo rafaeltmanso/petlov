ede144d382e1a4f11156fed832fbb92bf997ed3d
Rafael Manso <129555130+rafaeltmanso@users.noreply.github.com>
2025-10-22 15:08:22 -0300

    adding github actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  projectId: '4kub7r',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://petlov.vercel.app'
+    baseUrl: 'https://petlov.vercel.app',
   },
-};
+}
